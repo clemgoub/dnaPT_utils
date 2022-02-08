c58eabddee7300d2b33b382b2a12faa62774e832
clemgoub <goubert.clement@gmail.com>
2022-02-08 15:25:16 -0500

    add escapes on awk command

--- dnaPT_landscapes.sh
+++ dnaPT_landscapes.sh
@@ -21,7 +21,7 @@ function usage()
 
    This script perform at "TE landscape" analysis, i.e., it plots an histogram of the blastn divergence between raw reads (TE
    copies in the genomes) and their consensus sequences assembled in "Trinity.fasta". The script plots only putative TE seque
-   -nce among "LINE", "SINE", "LTR", "DNA" and "Unknown". 
+   -nce among "LINE", "SINE", "LTR", "DNA" and "Unknown" (a.k.a. "NA"). 
 
    Dependencies:
    - R + package "ggplot2" (https://www.r-bloggers.com/2010/11/installing-r-packages/)
@@ -213,7 +213,7 @@ invisible(lapply(packages, library, character.only = TRUE))
 # read main table and shell variables
 print("load variables...")
 # join the reads with annotations and format table for R
-land<-read.table(sep = "\t", text=system("join -a1 -12 -21 -o 1.3,2.4,2.5  $DSA/Annotation/sorted_blast3 $DSA/Annotation/one_RM_hit_per_Trinity_contigs | awk '/LINE/ { print $0 \"\\t\" $3; next} /LTR/ {print $0 \"\\t\" $3; next} /SINE/ {print $0 \"\\tSINE\"; next} /DNA/ {print $0 \"\\tDNA\"; next} /MITE/ {print $0 \"\\tMITE\";next} {if (NF == 3) {print $0\"\tOthers\"} else {print $0\"\\tNA\\tUnknown\\tUnknown\"}}' | sed 's/ /\t/g;s/\t\t\t/\\t/g' ", intern = T))
+land<-read.table(sep = "\t", text=system("join -a1 -12 -21 -o 1.3,2.4,2.5  $DSA/Annotation/sorted_blast3 $DSA/Annotation/one_RM_hit_per_Trinity_contigs | awk '/LINE/ { print \$0 \"\\t\" \$3; next} /LTR/ {print \$0 \"\\t\" \$3; next} /SINE/ {print \$0 \"\\tSINE\"; next} /DNA/ {print \$0 \"\\tDNA\"; next} /MITE/ {print \$0 \"\\tMITE\";next} {if (NF == 3) {print \$0\"\tOthers\"} else {print \$0\"\\tNA\\tUnknown\\tUnknown\"}}' | sed 's/ /\t/g;s/\t\t\t/\\t/g' ", intern = T))
 reads.c<-as.numeric(system("grep -c '>' $DSA/renamed.blasting_reads.fasta", intern = T))
 
 # pick the colors
